c42d96789b6904e3c25b82d245578cc88ef7c091
haoxr <1490493387@qq.com>
2020-12-14 15:54:10 +0800

    feat:q49

---
+++ src/main/java/com/leetcode/数据结构/哈希表/q49_字母异位词分组.java
@@ -0,0 +1,49 @@
+package com.leetcode.数据结构.哈希表;
+
+
+import java.util.*;
+
+//给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+//
+// 示例:
+//
+// 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+//输出:
+//[
+//  ["ate","eat","tea"],
+//  ["nat","tan"],
+//  ["bat"]
+//]
+//
+// 说明：
+//
+//
+// 所有输入均为小写字母。
+// 不考虑答案输出的顺序。
+//
+// Related Topics 哈希表 字符串
+// 👍 562 👎 0
+public class q49_字母异位词分组 {
+
+    public static void main(String[] args) {
+
+        String[] arr = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+
+        List<List<String>> lists = groupAnagrams(arr);
+        System.out.println("分组后的结果：" + lists.toString());
+    }
+
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
